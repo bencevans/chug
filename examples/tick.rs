b33fcac979fe8024eee22be9e1f4492b88f75aaa
Ben Evans <ben@bluechimp.io>
2023-01-10 15:50:57 +0000

    Base prediction on median to mitigate outliers (#3)

--- examples/tick.rs
+++ examples/tick.rs
@@ -3,7 +3,7 @@ use chug::Chug;
 fn main() {
     let mut chug = Chug::new(10, 100);
 
-    for _ in 0..100 {
+    for i in 0..100 {
         let formatted_eta = match chug.eta() {
             Some(eta) => {
                 let eta_secs = eta.as_secs();
@@ -13,6 +13,11 @@ fn main() {
             None => "ETA: None".to_string(),
         };
         println!("{}", formatted_eta);
+
+        if i == 0 {
+            // Checking that a long task doesn't affect the ETA much
+            std::thread::sleep(std::time::Duration::from_millis(1000));
+        }
 
         // Do some work
         std::thread::sleep(std::time::Duration::from_millis(50));
